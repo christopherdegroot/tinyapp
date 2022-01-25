e77ba39a25d72401e271381f54ef3114bf2e550b
christopherdegroot <christopher.neil.degroot@gmail.com>
2022-01-25 13:56:54 -0800

    added nodemon to manage server, linted and cleaned up code in express_server.js

--- express_server.js
+++ express_server.js
@@ -6,7 +6,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
-function generateRandomString() {
+let generateRandomString = function() {
   let result = '';
   let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   let charactersLength = characters.length;
@@ -31,23 +31,23 @@ app.get("/urls.json", (req, res) => {
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase};
-  res.render("urls_index", templateVars)
+  res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new")
+  res.render("urls_new");
 });
 
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
-  keyArray = [];
+  let keyArray = [];
   keyArray.push(generateRandomString());
   let newKey = keyArray[0];
 
   // console.log('logging newKey after pushing random string', newKey);
   urlDatabase[newKey] = `http://www.${req.body["longURL"]}`,
   
-  console.log(urlDatabase)
+  // console.log(urlDatabase)
   res.redirect(`urls/${newKey}`);         // Respond with 'Ok' (we will replace this)
 });
 
@@ -55,15 +55,14 @@ app.get("/urls/:shortURL", (req, res) => {
   // console.log(urlDatabase)
   // console.log(req.params);
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
-  res.render("urls_show", templateVars)
+  res.render("urls_show", templateVars);
 });
 
 app.get("/u/:shortURL", (req, res) => {
   // console.log('logging req.params', req.params)
-   const longURL = urlDatabase[req.params.shortURL]
+  const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
-
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
