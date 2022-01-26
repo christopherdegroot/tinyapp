4c8493cd9e4d8b24fe0fa0b61da9271d8854c132
christopherdegroot <christopher.neil.degroot@gmail.com>
2022-01-25 18:25:47 -0800

    Added username cookie functionality to express_server.js so that cookies are tracked along web app for a users given username.

--- express_server.js
+++ express_server.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser');
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 
 app.set("view engine", "ejs");
 
@@ -30,12 +32,15 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase};
+  const templateVars = { urls: urlDatabase, username: req.cookies.username};
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies.username,
+  };
+  res.render("urls_new", templateVars);
 });
 
 app.post("/urls", (req, res) => {
@@ -54,7 +59,7 @@ app.post("/urls", (req, res) => {
 app.get("/urls/:shortURL", (req, res) => {
   // console.log(urlDatabase)
   // console.log(req.params);
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies.username };
   res.render("urls_show", templateVars);
 });
 
@@ -90,6 +95,13 @@ app.post("/urls/:id", (req, res) =>{
   console.log('log urlDatabase after update', urlDatabase)
 
   res.redirect(`/urls`)
+});
+
+app.post("/login", (req, res) => {
+// console.log(req.params);
+// console.log(req.body.username);
+res.cookie('username', req.body.username);
+res.redirect(`/urls`)
 });
 
 
