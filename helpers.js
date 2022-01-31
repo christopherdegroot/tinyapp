86c61ed228af211e898c931cd6046a2cd2804ce8
christopherdegroot <christopher.neil.degroot@gmail.com>
2022-01-30 23:45:30 -0800

    added helper functions still in express server to helpers folder and removed from express_server file

--- helpers.js
+++ helpers.js
@@ -1,3 +1,4 @@
+//function to get a user by their email address
 const getUserByEmail = function(email, database) {
   for (let key in database) {
     // console.log(database[`${key}`].email)
@@ -7,4 +8,44 @@ const getUserByEmail = function(email, database) {
   };
 };
 
-module.exports = { getUserByEmail }
+//function to generate random string
+let generateRandomString = function(length) {
+  let result = '';
+  let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+};
+
+//function to lookup a user's email through their user ID
+let emailLookup = function(user_id, database) {
+  let userID = user_id;
+  let email = database[`${userID}`].email;
+  return email;
+};
+
+//function to check if a user is logged in
+let isLoggedIn = function(userID, database) {
+  if (database.hasOwnProperty(userID)) {
+    return true;
+  } else return false;
+};
+
+//function to check what urls in the global database a user has (uses user id as a parameter)
+let urlsForUser = function(id, urlDatabase) {
+  let filteredUrlDatabase = {};
+  for (let key in urlDatabase) {
+    if (urlDatabase[`${key}`].userID === id) {
+      filteredUrlDatabase[`${key}`] = {
+        longURL: urlDatabase[`${key}`].longURL,
+        userID: urlDatabase[`${key}`].userID
+      };
+    };
+  };
+  return filteredUrlDatabase;
+};
+
+
+module.exports = { getUserByEmail, getUserByEmail, generateRandomString, emailLookup, isLoggedIn, urlsForUser }
